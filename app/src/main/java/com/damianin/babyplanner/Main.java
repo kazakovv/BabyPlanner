9c13c4dfe8958016145f8b26968988d628f61334
kazakovv <kazakov.vcictor@gmail.com>
2015-05-24 17:44:24 +0100

    now working

--- app/src/main/java/com/damianin/babyplanner/Main.java
+++ app/src/main/java/com/damianin/babyplanner/Main.java
@@ -92,12 +92,7 @@ public class Main extends ActionBarActivity {
 
     */
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
